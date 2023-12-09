8a066955dad9f75ddcdcfd9c5287465cb74a6267
rpuwa <lilpushok@gmail.com>
2023-12-09 19:01:06 +0300

    init commit

---
+++ Scripts/database_init.sql
@@ -0,0 +1,36 @@
+-- schema creation
+drop schema if exists GenshinDB cascade;
+
+create schema GenshinDB;
+
+set search_path = GenshinDB;
+
+-- tables creation
+-- updates
+
+
+-- characters
+
+
+-- artifacts
+
+
+-- weapons
+
+
+-- elements
+
+
+-- countries
+
+
+-- units
+
+
+-- stats
+
+
+-- players
+
+
+-- skills
