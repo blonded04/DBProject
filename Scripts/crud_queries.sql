82238025e168371c8020047e07da9b92ed39fb2c
rpuwa <lilpushok@gmail.com>
2023-12-18 03:22:24 +0300

    they scared of six

--- Scripts/crud_queries.sql
+++ Scripts/crud_queries.sql
@@ -34,3 +34,36 @@ DELETE FROM countries WHERE _archont = 'Bebra';
 --
 -- task 6: motivated queries
 
+-- 1: Найти 3-его юнита по электро урону (берётся сумма всех электро навыков)
+
+
+-- 2: Максимальное значение хила от элементального навыка у юнитов из Инадзумы
+
+
+-- 3: Для каждой страны вычислить количество персонажей и юнитов
+SELECT countries._name country, COALESCE(counter.char_cnt, 0) characters_cnt, COALESCE(counter.unit_cnt, 0) units_cnt
+FROM countries
+LEFT JOIN 
+    (SELECT count(u._character) unit_cnt, count(DISTINCT c._name) char_cnt, _country 
+    FROM "characters" c 
+    LEFT JOIN units u ON u._character = c._name 
+    GROUP BY _country) counter ON countries._name = counter._country;
+
+-- 4: Для каждого персонажа вывести массив индексов его юнитов
+
+
+-- 5: Найти игрока, у которого достигается лучший бонус для гидро урона с помощью отряда
+
+
+-- 6: Найти юнитов, которые состоят из предметов, добавленных в течении одной мажорной версии игры (например, версии всех предметов вида 1.Х)
+SELECT u.* 
+FROM units u
+INNER JOIN (SELECT _name, _version FROM "characters" INNER JOIN updates ON _update = _id) ch ON u._character = ch._name
+INNER JOIN (SELECT _name, _version FROM weapons INNER JOIN updates ON _update = _id) w ON u._weapon = w._name
+INNER JOIN (SELECT _name, _version FROM artifacts INNER JOIN updates ON _update = _id) f ON u._flower_artifact = f._name
+INNER JOIN (SELECT _name, _version FROM artifacts INNER JOIN updates ON _update = _id) c ON u._clock_artifact = c._name
+INNER JOIN (SELECT _name, _version FROM artifacts INNER JOIN updates ON _update = _id) h ON u._hat_artifact = h._name
+WHERE CAST(ch._version AS INT) = CAST(w._version AS INT) AND
+    CAST(ch._version AS INT) = CAST(f._version AS INT) AND 
+    CAST(ch._version AS INT) = CAST(c._version AS INT) AND 
+    CAST(ch._version AS INT) = CAST(h._version AS INT);
